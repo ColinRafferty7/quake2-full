6a240c0b3be567479a91a6ab87eafab630e39f45
ColinRafferty7 <ctr6mince@gmail.com>
2025-04-25 20:33:22 -0400

    Trace now only detects world objects and is set to NULL if it does not collide with any

--- game/p_powers.c
+++ game/p_powers.c
@@ -8,15 +8,16 @@ void Geomancy(edict_t *self)
 	AngleVectors(self->client->v_angle, end, NULL, NULL);
 	VectorScale(end, 200, end);
 	VectorAdd(end, self->s.origin, end);
-
-	gi.cprintf(self, 2, "(%f, %f, %f)\n", self->s.origin[0], self->s.origin[1], self->s.origin[2]);
-	gi.cprintf(self, 2, "(%f, %f, %f)\n", end[0], end[1], end[2]);
 	
-	hitscan = gi.trace(self->s.origin, NULL, NULL, end, self, MASK_SHOT);
+	hitscan = gi.trace(self->s.origin, NULL, NULL, end, self, MASK_SOLID);
+
+	if (VectorCompare(hitscan.endpos, end))
+	{
+		hitscan.ent = NULL;
+	}
 
 	if (hitscan.ent)
 	{
 		gi.cprintf(self, 2, "%s\n", hitscan.ent->classname);
-		gi.cprintf(self, 2, "(%f, %f, %f)\n", hitscan.endpos[0], hitscan.endpos[1], hitscan.endpos[2]);
 	}
 }
