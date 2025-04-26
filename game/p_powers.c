f6a4215f722d7ee7eb95b0d6919d3b5ab9325bbf
ColinRafferty7 <ctr6mince@gmail.com>
2025-04-25 21:04:28 -0400

    Added extra trace for player convenience and a barrel now spawns where the trace collides with world objects

--- game/p_powers.c
+++ game/p_powers.c
@@ -1,23 +1,47 @@
 #include "g_local.h"
 
+void SpawnRock(edict_t* self, vec3_t origin)
+{
+	edict_t* rock;
+
+	rock = G_Spawn();
+
+	VectorCopy(origin, rock->s.origin);
+	rock->s.modelindex = gi.modelindex("models/objects/barrels/tris.md2");
+	gi.linkentity(rock);
+}
+
 void Geomancy(edict_t *self)
 {
-	vec3_t end;
+	vec3_t end1, end2;
 	trace_t hitscan;
 
-	AngleVectors(self->client->v_angle, end, NULL, NULL);
-	VectorScale(end, 200, end);
-	VectorAdd(end, self->s.origin, end);
+	AngleVectors(self->client->v_angle, end1, NULL, NULL);
+	VectorScale(end1, 200, end1);
+	VectorAdd(end1, self->s.origin, end1);
 	
-	hitscan = gi.trace(self->s.origin, NULL, NULL, end, self, MASK_SOLID);
+	hitscan = gi.trace(self->s.origin, NULL, NULL, end1, self, MASK_SOLID);
 
-	if (VectorCompare(hitscan.endpos, end))
+	if (VectorCompare(hitscan.endpos, end1))
 	{
-		hitscan.ent = NULL;
+		end2[2] = -1.0f;
+		VectorScale(end2, 100, end2);
+		VectorAdd(end2, end1, end2);
+
+		hitscan = gi.trace(end1, NULL, NULL, end2, self, MASK_SOLID);
+
+		if (VectorCompare(hitscan.endpos, end2))
+		{ 
+			hitscan.ent = NULL;
+		}
+
+		
 	}
 
 	if (hitscan.ent)
 	{
 		gi.cprintf(self, 2, "%s\n", hitscan.ent->classname);
+		SpawnRock(self, hitscan.endpos);
 	}
 }
+
